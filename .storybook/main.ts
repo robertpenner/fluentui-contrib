74c294da8be9ffb1a34859d0d094e92f64e6080d
Robert Penner <robert@robertpenner.com>
2026-09-11 18:42:30 -0400

    feat: load Fluent Inspector in shared Storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
     '@storybook/addon-docs',
     '@nx/react/plugins/storybook',
     '@storybook/addon-webpack5-compiler-babel',
+    '@fluentui-contrib/storybook-addon-fluent-inspector',
   ],
 
   framework: {
